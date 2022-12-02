59b7bf115746f8de23be8a45ecfaf2d483b9fc37
kaspeya <89647596+kaspeya@users.noreply.github.com>
2022-12-03 02:20:56 +0300

    delete comments in set

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -86,17 +86,6 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE',
-                            default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT')
-    }
-}
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
